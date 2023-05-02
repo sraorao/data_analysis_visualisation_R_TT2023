9691e9a9107f0c0399f30605986bc6c7c0491ce9
Srinivasa Rao Rao <sraorao@users.noreply.github.com>
2023-05-02 17:40:52 +0100

    Update data_vis_and_stats.R

--- Session3/data_vis_and_stats.R
+++ Session3/data_vis_and_stats.R
@@ -1,5 +1,5 @@
 ###R for biologists
-##Irina & Rao, 22/02/2023
+##Irina & Rao, 10/05/2023
 #Import and prepare the data ####
 library(tidyverse)
 #Import the all sheets from Excel file provided (use readxl)
